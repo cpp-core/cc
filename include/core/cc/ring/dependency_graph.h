4f14e1f6d337851d8bb7642e2bbbe07858b03bbd
Mark Melton <mark.melton@notlem.com>
2022-09-14 20:21:12 -0400

    Qualified string and string_view with std::.

--- include/core/cc/ring/dependency_graph.h
+++ include/core/cc/ring/dependency_graph.h
@@ -12,33 +12,33 @@ public:
     using Node = std::pair<string,Cursor*>;
     using Nodes = std::vector<Node>;
     using NodeMap = std::map<string,Cursor*>;
-    using AdjancencyMap = std::map<string,strings>;
+    using AdjancencyMap = std::map<string,std::vector<std::string>>;
     using SizeMap = std::map<string,size_t>;
 
     // Configure the dependency graph based on <graph>.
     void configure(const nlj::json& graph);
 
     // Return the size for the cursor <id>.
-    size_t size(const string& id) const;
+    size_t size(const std::string& id) const;
 
     // Return the size for the i'th source for <id>.
-    size_t size(const string& id, int i) const;
+    size_t size(const std::string& id, int i) const;
 
     // Return the source <id>'s for the given <id>.
-    strings sources(const string& id) const;
+    std::vector<std::string> sources(const std::string& id) const;
 
     // Return the i'th source <id> for the given <id>.
-    string source(const string& id, int i) const;
+    std::string source(const std::string& id, int i) const;
 
     // Register <cursor> with the given <id>.
-    void register_cursor(Cursor *cursor, const string& id);
+    void register_cursor(Cursor *cursor, const std::string& id);
 
     // Blocks until all cursors have been registered.
     void wait_for_all_registrations();
 
     // Apply the configured read and write barriers to <processor>
     template<class P>
-    void apply_barriers(P& processor, const string& id) {
+    void apply_barriers(P& processor, const std::string& id) {
 	wait_for_all_registrations();
 	if (auto iter = read_depends_.find(id); iter != read_depends_.end())
 	    for (auto c : iter->second)
@@ -50,7 +50,7 @@ public:
 
 private:
     // Return the cursor for the given <id> or throw an exception.
-    Cursor *get_cursor(const string& id);
+    Cursor *get_cursor(const std::string& id);
 
     std::mutex mutex_;
     std::condition_variable cond_;
